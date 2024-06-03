2a466faabe50295dc67c3fc433b51fd9d75aa3c0
whitetommy <1228tommy@naver.com>
2024-06-04 00:30:56 +0900

    C:/Program Files/Git/feat 웹페이지 이름, 아이콘추가& blog update 개선

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -10,11 +10,13 @@ export async function GET(req) {
         isPublic: true,
       },
     });
-    return new NextResponse.json(posts, { status: 200 });
+    return NextResponse.json(posts, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to fetch projects" },
       { status: 500 }
     );
+  } finally {
+    await prisma.$disconnect(); // Prisma 클라이언트 연결을 닫기
   }
 }
